61a62e258c9028a8e01ab3648ec6597feb3e0a81
Alexis Gallegos <98421465+galle997@users.noreply.github.com>
2022-04-03 18:05:57 -0600

    archivo app.blade.php que recibe {{$slot}} no se borra

--- resources/views/livewire/show.blade.php
+++ resources/views/livewire/show.blade.php
@@ -4,15 +4,19 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>{{ $alumnos->id }}</h2>
+                <h2>{{$alumno->id}}</h2>
             </div>
         </div>
+        @include('livewire.show')   
     </div>
+    @foreach ($alumnos as $alumno )
+        
+    
     <div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Nombre:</strong>
-                {{ $alumnos->nombre }}
+                {{$alumno->name}}
             </div>
         </div>
     </div>
@@ -20,7 +24,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Codigo:</strong>
-                {{ $alumnos->code }}
+                {{$alumno->code}}
             </div>
         </div>
     </div>
@@ -28,7 +32,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Dirección:</strong>
-                {{ $alumnos->direccion }}
+                {{$alumno->direccion}}
             </div>
         </div>
     </div>
@@ -36,16 +40,17 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Telefono:</strong>
-                {{ $alumnos->n_tel }}
+                {{$alumno->n_tel}}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Correo:</strong>
-                {{ $alumnos->correo }}
+                {{$alumno->correo}}
             </div>
         </div>
     </div>
+    @endforeach
 
             <div class="pull-right">
             <a href="{{ URL::to('alumnos/create') }}" class="btn btn-warning">Añadir Alumno<i class="fas fa-plus-circle"></i></a>
